22fe27279b68e6d76a338964cec8f295abb7e58c
MateusX02 <89735765+MateusX02@users.noreply.github.com>
2022-02-04 21:24:20 -0300

    NE

--- source/FlxVideo.hx
+++ source/FlxVideo.hx
@@ -1,132 +1,64 @@
-#if web
-import openfl.net.NetConnection;
-import openfl.net.NetStream;
-import openfl.events.NetStatusEvent;
-import openfl.media.Video;
-#else
-import openfl.events.Event;
-#end
-import flixel.FlxBasic;
+package;
+
+import flixel.FlxCamera;
+import Achievements;
+import flixel.text.FlxText;
+import flixel.FlxState;
 import flixel.FlxG;
 
-class FlxVideo extends FlxBasic {
-	#if VIDEOS_ALLOWED
-	public var finishCallback:Void->Void = null;
-	
-	#if desktop
-	public static var vlcBitmap:VlcBitmap;
-	#end
+import extension.webview.WebView;
 
-	public function new(name:String) {
+using StringTools;
+
+class VideoState extends MusicBeatState
+{
+	public static var SUSPATH:String = 'file:///';
+
+	public var nextState:FlxState;
+
+	var text:FlxText;
+
+	private var camAchievement:FlxCamera;
+
+	public function new(source:String, toTrans:FlxState)
+	{
 		super();
 
-		#if web
-		var player:Video = new Video();
-		player.x = 0;
-		player.y = 0;
-		FlxG.addChildBelowMouse(player);
-		var netConnect = new NetConnection();
-		netConnect.connect(null);
-		var netStream = new NetStream(netConnect);
-		netStream.client = {
-			onMetaData: function() {
-				player.attachNetStream(netStream);
-				player.width = FlxG.width;
-				player.height = FlxG.height;
-			}
-		};
-		netConnect.addEventListener(NetStatusEvent.NET_STATUS, function(event:NetStatusEvent) {
-			if(event.info.code == "NetStream.Play.Complete") {
-				netStream.dispose();
-				if(FlxG.game.contains(player)) FlxG.game.removeChild(player);
-
-				if(finishCallback != null) finishCallback();
-			}
-		});
-		netStream.play(name);
-
-		#elseif desktop
-		// by Polybius, check out PolyEngine! https://github.com/polybiusproxy/PolyEngine
-
-		vlcBitmap = new VlcBitmap();
-		vlcBitmap.set_height(FlxG.stage.stageHeight);
-		vlcBitmap.set_width(FlxG.stage.stageHeight * (16 / 9));
-
-		vlcBitmap.onComplete = onVLCComplete;
-		vlcBitmap.onError = onVLCError;
-
-		FlxG.stage.addEventListener(Event.ENTER_FRAME, fixVolume);
-		vlcBitmap.repeat = 0;
-		vlcBitmap.inWindow = false;
-		vlcBitmap.fullscreen = false;
-		fixVolume(null);
-
-		FlxG.addChildBelowMouse(vlcBitmap);
-		vlcBitmap.play(checkFile(name));
-		#end
-	}
-
-	#if desktop
-	function checkFile(fileName:String):String
-	{
-		var pDir = "";
-		var appDir = "file:///" + Sys.getCwd() + "/";
-
-		if (fileName.indexOf(":") == -1) // Not a path
-			pDir = appDir;
-		else if (fileName.indexOf("file://") == -1 || fileName.indexOf("http") == -1) // C:, D: etc? ..missing "file:///" ?
-			pDir = "file:///";
-
-		return pDir + fileName;
-	}
+		camAchievement = new FlxCamera();
+		camAchievement.bgColor.alpha = 0;
 	
-	public static function onFocus() {
-		if(vlcBitmap != null) {
-			vlcBitmap.resume();
-		}
-	}
-	
-	public static function onFocusLost() {
-		if(vlcBitmap != null) {
-			vlcBitmap.pause();
-		}
-	}
+		FlxG.cameras.add(camAchievement);
 
-	function fixVolume(e:Event)
-	{
-		// shitty volume fix
-		vlcBitmap.volume = 0;
-		if(!FlxG.sound.muted && FlxG.sound.volume > 0.01) { //Kind of fixes the volume being too low when you decrease it
-			vlcBitmap.volume = FlxG.sound.volume * 0.5 + 0.5;
-		}
-	}
-
-	public function onVLCComplete()
-	{
-		vlcBitmap.stop();
-
-		// Clean player, just in case!
-		vlcBitmap.dispose();
-
-		if (FlxG.game.contains(vlcBitmap))
 		{
-			FlxG.game.removeChild(vlcBitmap);
+			text = new FlxText(0, 0, 0, "APERTE ENTER\nPARA CONTINUAR", 48);
+			text.screenCenter();
+			text.alpha = 0;
+			add(text);
 		}
 
-		if (finishCallback != null)
-		{
-			finishCallback();
-		}
+
+		nextState = toTrans;
+
+		WebView.onClose=onClose;
+		WebView.onURLChanging=onURLChanging;
+
+		WebView.open(SUSPATH + source + '.html', false, null, ['http://exitme(.*)']);
 	}
 
-	
-	function onVLCError()
-		{
-			//trace("An error has occured while trying to load the video.\nPlease, check if the file you're loading exists.");
-			if (finishCallback != null) {
-				finishCallback();
-			}
-		}
-	#end
-	#end
+	public override function update(dt:Float) {
+		if(FlxG.keys.justPressed.ENTER)
+				onClose(); //hmmmm maybe
+
+		super.update(dt);	
+	}
+
+	function onClose(){// not working
+		text.alpha = 0;
+		MusicBeatState.switchState(nextState);
+	}
+
+	function onURLChanging(url:String) {
+		text.alpha = 1;
+		if (url == 'http://exitme(.*)') onClose(); // drity hack lol
+	}
 }
